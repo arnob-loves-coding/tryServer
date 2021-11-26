1e8758ae5d742f97c9258de375b0b5e631f11966
arnob-loves-coding <arnobroy-13-2018623891@eng.du.ac.bd>
2021-11-26 22:37:36 +0600

    edited

--- index.js
+++ index.js
@@ -12,24 +12,94 @@ app.use(cors());
 app.use(express.json());
 
 // connect to database
-// const uri = 'mongodb+srv://animesh:GTVWtbr9d695ibZg@cluster0.wa3vk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const uri =
+  "mongodb+srv://animesh:GTVWtbr9d695ibZg@cluster0.wa3vk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tryServer");
+    const collection = database.collection("crudOperation");
+    //get api
+    app.get("/users", async (req, res) => {
+      const result = await collection.find({}).toArray();
+      res.json(result);
+    });
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await collection.findOne(query);
+      res.json(result);
+    });
+    // PUT api
+    app.put("/users/:id", async (req, res) => {
+      const info = req.body;
+      console.log(info);
+      const filter = { _id: ObjectId(req.params.id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: info.name,
+          email: info.email,
+        },
+      };
+      const result = collection.updateOne(filter, updateDoc, options);
+    });
+    // post api
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await collection.insertOne(newUser);
+      res.json(result);
+    });
 
-const users = [
-  {
-    id: 1,
-    name: "shabana",
-    email: "shabana@gmail.com",
-  },
-  {
-    id: 2,
-    name: "shabnur",
-    email: "shabnur@gmail.com",
-  },
-];
+    // //put api
+    // app.put("/users/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const updatedUser = req.body;
+    //   console.log(id, updatedUser, "hitting update");
+
+    //   const filter = { _id: ObjectId(id) };
+
+    //   const options = { upsert: true };
+
+    //   const updateDoc = {
+    //     $set: {
+    //       name: updatedUser.name,
+    //       email: updatedUser.email,
+    //     },
+    //   };
+    //   const result = await collection.updateOne(filter, updateDoc, options);
+    //   console.log(result);
+    //   res.json(result);
+    // });
+    //delete api
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await collection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// const users = [
+//   {
+//     id: 1,
+//     name: "shabana",
+//     email: "shabana@gmail.com",
+//   },
+//   {
+//     id: 2,
+//     name: "shabnur",
+//     email: "shabnur@gmail.com",
+//   },
+// ];
 
 app.get("/", (req, res) => {
   res.json("server running");
@@ -39,44 +109,32 @@ app.get("/", (req, res) => {
 // app.get("/users", (req, res) => {
 //   const search = req.query.search;
 //   if (search) {
-//     const searchResult = users.filter((user) => {
-//       user.name.toLocaleLowerCase().includes(search);
-//     });
+//     const searchResult = users.filter((user) =>
+//       user.name.toLocaleLowerCase().includes(search)
+//     );
 //     res.send(searchResult);
-//     console.log(users);
+//     console.log(search);
 //   } else {
 //     res.send(users);
 //   }
 // });
-app.get("/users", (req, res) => {
-  const search = req.query.search;
-  if (search) {
-    const searchResult = users.filter((user) =>
-      user.name.toLocaleLowerCase().includes(search)
-    );
-    res.send(searchResult);
-    console.log(search);
-  } else {
-    res.send(users);
-  }
-});
-app.post("/users", (req, res) => {
-  const newUser = req.body;
-  newUser.id = users.length + 1;
-  users.push(newUser);
-  console.log("post hitting", req.body, users.length);
-  res.json(newUser);
-});
+// app.post("/users", (req, res) => {
+//   const newUser = req.body;
+//   newUser.id = users.length + 1;
+//   users.push(newUser);
+//   console.log("post hitting", req.body, users.length);
+//   res.json(newUser);
+// });
 
-app.get("/fruits/mangoes/fazley", (req, res) => {
-  res.send("yummy yummu fazley");
-});
-app.get("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const user = users[id];
-  console.log(user);
-  res.send(user);
-});
+// app.get("/fruits/mangoes/fazley", (req, res) => {
+//   res.send("yummy yummu fazley");
+// });
+// app.get("/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   const user = users[id];
+//   console.log(user);
+//   res.send(user);
+// });
 app.listen(port, () => {
   console.log("server running at port:", port);
 });
